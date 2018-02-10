aefd3db088ce1502c037b8b258a336ce2e497e98
DJ Sanderson <djsanderson1@gmail.com>
2018-02-10 14:51:38 -0700

    10 sec wait and prevent back button

--- standard.php
+++ standard.php
@@ -1,12 +1,26 @@
 <!doctype html>
 <html>
 <head>
+  <script type = "text/javascript" >
+   function preventBack(){window.history.forward();}
+    setTimeout("preventBack()", 0);
+    window.onunload=function(){null};
+  </script>
   <style>
     button {
       padding: 10px;
       font-size: 25pt;
       width: 400px;
     }
+    .noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+}
   </style>
   <title>Phone Calls View</title>
   <?php include 'style.php'; ?>
@@ -24,6 +38,7 @@ if(isset($_GET["status_id"]) && isset($_GET["resident_id"])) {
     last_called_date = date(now())
     WHERE resident_id = " . $_GET['resident_id']
   );
+  sleep(10);
 }
 $res=$con->query("
 SELECT *
@@ -45,6 +60,7 @@ while ($row = $res->fetch_assoc()) {
     <button type="button" onclick="result = confirm(' . "'Are you sure that " . $row["phone_number"] . "is a do not call?'" . '); if(result){location.href=' . "'standard.php?status_id=3&resident_id=" . $row["resident_id"] . "'" . '}">Do Not Call</button><br>
     <button type="button" onclick="result = confirm(' . "'Are you sure that " . $row["phone_number"] . "was contacted?'" . '); if(result){location.href=' . "'standard.php?status_id=4&resident_id=" . $row["resident_id"] . "'" . '}">Contacted</button><br>
     <button type="button" onclick="result = confirm(' . "'Are you sure that " . $row["phone_number"] . "is a foreign language?'" . '); if(result){location.href=' . "'standard.php?status_id=5&resident_id=" . $row["resident_id"] . "'" . '}">Foreign Language</button><br>
+    <a href="standard.php" style="color:black;cursor:default;" class="noselect">Skip to Next</a>
   ';
 }
 ?>
