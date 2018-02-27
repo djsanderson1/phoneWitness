bb45c7e9f731672941173201a14e34cc9e9b1c4a
DJ Sanderson <djsanderson1@gmail.com>
2018-02-26 20:36:29 -0700

    This file has errors, not ready yet.

--- territories.php
+++ territories.php
@@ -23,9 +23,25 @@
     include 'mysqlConnect.php';
     $res=$con->query("SELECT * FROM territories");
     while ($row = $res->fetch_assoc()) {
+
+      if(isset($_GET['territory_id'])) {
+        if($row['territory_id']) == isset($_GET['territory_id']) {
+            $thisTerritoryRefreshed = True;
+            $refreshedRowStyle = "background-color: #ccc;";
+            $refreshedMessage = " - Territory Refreshed! Stay Fresh!";
+          } else {
+            $thisTerritoryRefreshed = False;
+            $refreshedRowStyle = "";
+            $refreshedMessage = "";
+          }
+        } else {
+          $thisTerritoryRefreshed = False;
+          $refreshedRowStyle = "";
+          $refreshedMessage = "";
+        }
       echo '
-      <tr>
-        <td><a href="territoryDetails.php?territory_id=' . $row["territory_id"] . '">' . $row["territory_number"] . '</a></td>
+      <tr style="$refreshedRowStyle">
+        <td><a href="territoryDetails.php?territory_id=' . $row["territory_id"] . '">' . $row["territory_number"] . $refreshedMessage . '</a></td>
         <td>' . $row["last_import_date"] . '</td>
         <td>' . $row["last_worked_date"] . '</td>
         <td><a href="' . $row["territoryImageUrl"] . '"><img src="' . $row["territoryImageUrl"] . '" height="50"></a></td>
