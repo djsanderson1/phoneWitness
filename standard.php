d323c5d7aa629e18f4352fd358e919885d34b6cc
DJ <djsanderson1@gmail.com>
2018-05-22 19:00:01 -0600

    Revert "Revert "Revert "Need to go back to this and fix it"""
    This reverts commit 66394a0d2e71eec3da6cdbf3069bae2c0d0ad88d.

--- standard.php
+++ standard.php
@@ -26,6 +26,7 @@
   ?><br><br>
   Phone numbers ready to call:
   <?php
+    include 'mysqlConnect.php';
     $res=$con->query("
     select count(*) AS ready_to_call from residents where (status_id IN(1,2) OR status_id IS NULL) AND phone_number IS NOT NULL AND phone_number <> ''
         ");
