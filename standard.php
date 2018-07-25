0c163eebcfb60c19864556114bf5eb5c738dac7a
DJ <djsanderson1@gmail.com>
2018-07-25 14:30:51 -0600

    counts were wrong, fixed

--- standard.php
+++ standard.php
@@ -32,7 +32,7 @@
     where (status_id IN(1,2) OR status_id IS NULL) AND phone_number IS NOT NULL AND phone_number <> ''
     AND territory_queue.order_number > 0
     AND number_of_tries < 3
-    AND status_id2 NOT IN(3,6)
+    AND status_id2 IS NULL
         ");
     while ($row = $res->fetch_assoc()) {
       echo $row["ready_to_call"];
