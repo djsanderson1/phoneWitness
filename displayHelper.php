db7f0ac98e2c56e0a184f078393e9d823bb53970
DJ <djsanderson1@gmail.com>
2018-08-12 17:21:33 -0600

    Created getVar function
    The getVar function can be used to safely get the value of any variable as long as the name is unique. It will check to make sure if the variable is a Get, Post or normal variable and confirm that it exists at all to prevent any errors. If it doesn't exist at all, it will set the value to whatever you set as the blank value, which is the second parameter this function accepts. This should save me a lot of time.

--- displayHelper.php
+++ displayHelper.php
@@ -3,4 +3,22 @@ function html2specialchars($str){
     $trans_table = array_flip(get_html_translation_table(HTML_ENTITIES));
     return strtr($str, $trans_table);
 }
+function getVar($str, $blankValue) {
+  if (isset($_GET[$str])) {
+      return $_GET($str);
+  }
+  else {
+    if(isset($_POST[$str])) {
+      return $_POST[$str];
+    }
+    else {
+      if(isset(${$str})) {
+        return ${$str};
+      }
+      else {
+        return $blankValue;
+      }
+    }
+  }
+}
 ?>
