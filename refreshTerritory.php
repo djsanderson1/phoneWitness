2e40afda034c4abd4b9756dbd93dab02c00ee646
DJ <djsanderson1@gmail.com>
2019-05-18 10:16:00 -0600

    Fixed refreshTerritory.php for exports

--- refreshTerritory.php
+++ refreshTerritory.php
@@ -6,7 +6,8 @@ require_once('authenticate.php');
     UPDATE residents
     SET status_id = NULL,
     last_called_date = NULL,
-    number_of_tries = NULL
+    number_of_tries = NULL,
+    address_export_id = NULL
     WHERE territory_id = " . $_GET['territory_id']
   );
   header('Location: territories.php?refreshed='.$_GET['territory_id']);
