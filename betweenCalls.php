c5fbd2c84c5050c38bca810c65284e171765d4ce
DJ <djsanderson1@gmail.com>
2020-03-28 18:09:47 -0600

    Countdown of seconds shows up and always happens after login or refresh.

--- betweenCalls.php
+++ betweenCalls.php
@@ -7,10 +7,11 @@
     <?php include 'style.php'; ?>
   </head>
   <script>
+    var currentCount = 45;
     function timedPhoneCall() {
       document.getElementById("activityPage").style.display = "none";
       document.getElementById("waitMsg").style.display = "inline";
-      setTimeout(makePhoneCall, 30000);
+      window.countdownInterval = setInterval(phoneCallCountDown, 1000);
     }
     function makePhoneCall() {
       document.getElementById("showNext").style.display = "inline";
@@ -23,9 +24,22 @@
     function reloadData() {
       $( "#activityPage" ).load( "activity.php" );
     }
+    function phoneCallCountDown() {
+      var countDown = document.getElementById("countDown");
+      countDown.innerHTML = currentCount;
+      if(currentCount > 0) {
+        countDown.style.display = "inline";
+        currentCount--;
+      } else {
+        countDown.style.display = "none";
+        currentCount = 45;
+        clearInterval(window.countdownInterval);
+        makePhoneCall();
+      }
+    }
   </script>
-  <body>
-    <p id="waitMsg" style="display: none;">Please pass this device to the next person.</p>
+  <body onload="timedPhoneCall();">
+    <p id="waitMsg" style="display: none;">Please pass this device to the next person.<br><h1 id="countDown"></h1></p>
     <p id="showNext" style="display:none;">Click the link below when ready to make a call:<br><br>
       <a href="#" onclick="displayActivityPage(); playNotificationSound(); reloadData();">Make a Call</a>
     </p>
