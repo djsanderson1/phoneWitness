6023f7af1aceac32ae5fd009a988c5aac6736784
djsanderson1 <36252083+djsanderson1@users.noreply.github.com>
2020-04-04 22:44:56 -0600

    Export addresses only shows territories with at least one to export.

--- export_addresses.php
+++ export_addresses.php
@@ -35,21 +35,23 @@ require_once('functions/publishers/getPublishers.php');
     else {
     ?><form action="" method="get"><select name="territory_id"><?php
       require_once('mysqlConnect.php');
+      $available_exports_sql = "(select count(*) from residents
+      where
+      ((address_export_id IS NULL OR address_export_id = 0) AND (status_id2 <> 3 OR status_id2 IS NULL))
+                  AND (
+                    number_of_tries >= 3
+                    OR phone_number IS NULL
+                    OR phone_number = '')
+                  AND territory_id = territory_queue.territory_id)";
       $res=$con->query("
       SELECT territory_queue.territory_id, territory_queue.order_number, territories.territory_number territory_number,
-        (select count(*) from residents
-        where
-        ((address_export_id IS NULL OR address_export_id = 0) AND (status_id2 <> 3 OR status_id2 IS NULL))
-                    AND (
-                      number_of_tries >= 3
-                      OR phone_number IS NULL
-                      OR phone_number = '')
-                    AND territory_id = territory_queue.territory_id) AS available_exports
+        $available_exports_sql AS available_exports
 
         FROM territory_queue
         LEFT JOIN territories USING(territory_id)
 
        WHERE territory_queue.order_number > 0
+       AND $available_exports_sql > 0
        ORDER BY CAST(territory_number AS UNSIGNED)
        ");
       while ($row = $res->fetch_assoc()) {
