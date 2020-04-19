abdf5923937d25424064a81955b62cba8ad970e2
djsanderson1 <36252083+djsanderson1@users.noreply.github.com>
2020-04-19 14:59:52 -0600

    Made changes to sorting on territories.php
    THis is on the Terroritories list on admin. The problem is that if you 
    sort in descending order and then click another one, it's sorting in 
    descending instead of always starting with ascending for a new column. 
    Need to get that figured out to have it properly sort.

--- territories.php
+++ territories.php
@@ -13,16 +13,52 @@
     <table>
         <thead>
         <tr>
-          <th>Territory Number</th>
-          <th>Last Import Date</th>
-          <th>Last Worked Date</th>
-          <th>Territory Image</th>
+          <th><a href="?sortby=territory_number">Territory Number</a></th>
+          <th><a href="?sortby=last_import_date">Last Import Date</a></th>
+          <th><a href="?sortby=last_worked_date">Last Worked Date</a></th>
+          <th><a href="?sortby=territoryImageUrl">Territory Image</a></th>
           <th>Available to Export</th>
           <th>Action</th>
         </tr>
         </thead>
         <tbody>
     <?php
+    // default sorting
+    $defaultSortBy  = 'territory_number';
+    $defaultSortDir = 'ASC';
+
+    if(isset($_GET['sortby'])) {
+      $sortby = $_GET['sortby'];
+      if(isset($_SESSION['sortdir'])) {
+        $existingSortdir = $_SESSION['sortdir'];
+      } else {
+        $existingSortdir = '';
+      }
+      if($existingSortdir == '') {
+        $_SESSION['sortdir'] = $defaultSortDir;
+      }
+      if(isset($_SESSION['sortby'])) {
+        $existingSortby = $_SESSION['sortby'];
+      } else {
+        $existingSortby = '';
+      }
+      $_SESSION['sortby'] = $sortby;
+      $sortdir = $_SESSION['sortdir'];
+      if($sortby == $existingSortby && $sortdir == $existingSortdir) {
+        if($sortdir == 'ASC') {
+          $_SESSION['sortdir'] = 'DESC';
+        } else {
+          $_SESSION['sortdir'] = 'ASC';
+        }
+      }
+      $sortdir = $_SESSION['sortdir'];
+    } else {
+      $sortby = $defaultSortBy;
+      $sortdir = $defaultSortDir;
+    }
+    if($sortby == 'territory_number') {
+      $sortby = 'CAST(territory_number AS UNSIGNED)';
+    }
     include 'mysqlConnect.php';
     $res=$con->query("
       SELECT  territories.territory_id,
@@ -33,7 +69,9 @@
               territories.territory_status,
               territory_queue.order_number
         FROM  territories
-    INNER JOIN territory_queue USING(territory_id)");
+    INNER JOIN territory_queue USING(territory_id)
+    ORDER BY $sortby $sortdir
+    ");
     while ($row = $res->fetch_assoc()) {
 
       if(isset($_GET['refreshed'])) {
