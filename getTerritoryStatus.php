76cd87315ca859031528f1fa08ae92de0977474b
djsanderson1 <36252083+djsanderson1@users.noreply.github.com>
2020-10-03 12:36:52 -0600

    WIP - not ready
    This is still very much in the early stages, but figured out the proper 
    methodology on this. I will have to use jquery post command to get this 
    working. Tried to get curl to work, but it's fighting me but when I do a 
    normal form submit, it does create the JSON data that I need. That means 
    that I need to use jQuery to grab that, pull it into a local hidden form 
    and then submit it. This will need to be done every time we do a 
    download. 
    
    I think that while the token only lasts for 3600 secs, the code and 
    other data will last longer. I think we will want to have it save the 
    code, redirect_uri, client_id, and client_secret fields all in a table 
    as those won't change often. It's a little weird to do, but we may have 
    to then run a local batch file to open the page on the site so that the 
    jQuery will run because jQuery requires a browser.

--- getTerritoryStatus.php
+++ getTerritoryStatus.php
@@ -2,12 +2,66 @@
 /* This page will need to be run for updates
 
 */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once('authenticate.php');
 require_once('functions/api/territoryHelper.php');
+/*
 $client_id = "4edf19f4e7aa4391aeb0b15399b7c895";
 $client_secret = "61f6462b01e5453e982ac616e4fd5050";
-$redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+*/
+$client_id = "e2a4ca79d56a4de8a078746d8c61319b";
+$client_secret = "8930b0e816a648ebaa09ea4e60baddc8";
+$redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].'/getTerritoryStatus.php';
 $url = territoryHelperAuthLink($client_id, $redirect_uri);
 $code = getGet("code");
 echo '<a href="'.$url.'">Authenticate</a><br>code: '.$code;
+echo '<br><a href="'.$redirect_uri.'">Reset</a>';
+if($code != "") {
+
+  echo '
+  <form action="https://territoryhelper.com/api/token" method="post">
+    <input type="text" name="grant_type" value="authorization_code">
+    <input type="text" name="code" value="'.$code.'">
+    <input type="text" name="redirect_uri" value="'.$redirect_uri.'">
+    <input type="text" name="client_id" value="'.$client_id.'">
+    <input type="text" name="client_secret" value="'.$client_secret.'">
+    <input type="submit">
+  </form>';
+  /*
+  echo 'test';
+  //The url you wish to send the POST request to
+$url = 'https://territoryhelper.com/api/token';
+
+//The data you want to send via POST
+$fields = [
+    'grant_type'    => 'authorization_code',
+    'code'          => $code,
+    'redirect_uri'  => $redirect_uri,
+    'client_id'     => $client_id,
+    'client_secret' => $client_secret
+];
+
+//url-ify the data for the POST
+$fields_string = http_build_query($fields);
+
+//open connection
+$ch = curl_init($url);
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+
+//So that curl_exec returns the contents of the cURL; rather than echoing it
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//execute post
+$result = curl_exec($ch);
+curl_close($ch);
+echo 'mytest';
+echo $result;
+*/
+}
  ?>
