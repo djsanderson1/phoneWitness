2bb343b90ba4bcf09c0854200d7c93b3c702ddc3
djsanderson1 <36252083+djsanderson1@users.noreply.github.com>
2020-11-07 10:23:43 -0700

    Fixed territory refresh.

--- refreshTerritory.php
+++ refreshTerritory.php
@@ -5,11 +5,13 @@ require_once('authenticate.php');
   $con->query("
     UPDATE residents
     SET status_id = NULL,
-    IF(status_id2 <> 3 AND status_id <> 3, last_called_date = NULL,)
+    last_called_date = NULL,
     number_of_tries = NULL,
     address_export_id = NULL
-    WHERE territory_id = " . $_GET['territory_id']
+    WHERE (status_id2 <> 3 or status_id2 IS NULL)
+      and (status_id <> 3 or status_id IS NULL)
+      AND territory_id = " . $_GET['territory_id']
   );
-  // header('Location: territories.php?refreshed='.$_GET['territory_id']);
+  header('Location: territories.php?refreshed='.$_GET['territory_id']);
 }
 ?>
