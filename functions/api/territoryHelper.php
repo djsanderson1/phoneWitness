faaec2ea304f36a1da7ebcff3a2d3b4991c01676
djsanderson1 <36252083+djsanderson1@users.noreply.github.com>
2021-01-02 13:58:23 -0700

    WIP - added some php functions for getting the client id, secret and redirects.
    This will be useful later especially when we start adding multiple 
    congregations. In the future we will want this data to come from the 
    database from the settings table based on the congregation.

--- functions/api/territoryHelper.php
+++ functions/api/territoryHelper.php
@@ -1,4 +1,13 @@
 <?php
+function getThClientID() {
+  return "e2a4ca79d56a4de8a078746d8c61319b";
+}
+function getThClientSecret() {
+  return "8930b0e816a648ebaa09ea4e60baddc8";
+}
+function getThRedirectUri() {
+  return 'http://'.$_SERVER['HTTP_HOST'].'/getTerritoryStatus.php';
+}
 function territoryHelperAuthLink($client_id, $redirect_uri) {
   return "https://territoryhelper.com/api/auth?response_type=code&client_id=$client_id&redirect_uri=$redirect_uri";
 }
